6b6df3c9a949249b1ac26854dd610195e1005eb5
atesz014 <atesz014@gmail.com>
2023-03-14 15:33:00 +0100

    Add project files.

---
+++ Dashboard/Models/MenuItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Dashboard.Models
+{
+    public class MenuItem
+    {
+        public string? Link { get; set; }
+        public string? NormalClass { get; set; }
+        public string? NormalTitle { get; set; }
+        public string? MiniClass { get; set; }
+        public string? MiniTitle { get; set; }
+    }
+}
+
+
