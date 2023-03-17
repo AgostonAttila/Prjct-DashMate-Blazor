c4b1b89e73efe8f76578dd3b0445c07015141c08
atesz014 <atesz014@gmail.com>
2023-03-17 13:23:00 +0100

    before appstate

---
+++ Dashboard/StateContainers/AppState.cs
@@ -0,0 +1,55 @@
+﻿namespace Dashboard.StateContainers
+{
+	public class AppState
+	{
+
+		//public bool IsShowSideBar { get; private set; }
+		//public string? SideBarColor { get; private set; }
+		//public string? SideBarType { get; private set; }
+		//public bool NavbarFixed { get; private set; }
+		//public bool NavbarMinimize { get; private set; }
+		//public bool DarkMode { get; private set; }
+
+		//public event Action OnChange;
+
+
+
+		//public void SetSideBarColor(string color)
+		//{
+		//	SideBarColor = color;
+		//	NotifyStateChanged();
+		//}
+
+		//public void SetIsShowSideBar(bool isShow)
+		//{
+		//	IsShowSideBar = isShow;
+		//	NotifyStateChanged();
+		//}
+
+		//public void SetSideBarType(string type)
+		//{
+		//	SideBarType = type;
+		//	NotifyStateChanged();
+		//}
+
+		//public void SetNavbarFixed(bool isFixed)
+		//{
+		//	NavbarFixed = isFixed;
+		//	NotifyStateChanged();
+		//}
+
+		//public void SetNavbarMinimize(bool isMinimizeNavBar)
+		//{
+		//	NavbarMinimize = isMinimizeNavBar;
+		//	NotifyStateChanged();
+		//}
+
+		//public void SetDarkMode(bool isDarkMode)
+		//{
+		//	DarkMode = isDarkMode;
+		//	NotifyStateChanged();
+		//}
+
+		//private void NotifyStateChanged() => OnChange?.Invoke();
+	}
+}
