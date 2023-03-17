9d1e8a0cd0530c32452cd33e65b9c917be27df94
atesz014 <atesz014@gmail.com>
2023-03-17 23:27:32 +0100

    sidbear colors /type finish theme selector

--- Dashboard/StateContainers/AppState.cs
+++ Dashboard/StateContainers/AppState.cs
@@ -4,17 +4,22 @@
 	{
 
 		public bool IsShowThemeSelector { get; private set; }
+		
+		
 		public string? SideBarColor { get; private set; }
 		public string? SideBarType { get; private set; }
-		public bool SideBarMinimize { get; private set; } 
-		public bool NavbarFixed { get; private set; }		
+		public bool SideBarMinimize { get; private set; }
+		public bool NavBarFixed { get; private set; }
 		public bool DarkMode { get; private set; }
+
+
+		public string NavBarSelectedMenus { get; private set; }
+		
 
 		public event Action OnChange;
 
 
-
-		//SideBar
+		#region SideBar
 		public void SetSideBarColor(string color)
 		{
 			SideBarColor = color;
@@ -28,16 +33,16 @@
 		}
 
 		public void SetSideBarMinimize()
-		{			
+		{
 			SideBarMinimize = !SideBarMinimize;
 			NotifyStateChanged();
 		}
 
 		public void SetNavbarFixed()
 		{
-			NavbarFixed = !NavbarFixed;
+			NavBarFixed = !NavBarFixed;
 			NotifyStateChanged();
-		}		
+		}
 
 		public void SetDarkMode()
 		{
@@ -47,13 +52,23 @@
 			NotifyStateChanged();
 		}
 
+		#endregion
 
-		//ThemeSelector
+		#region ThemeSelector
 		public void SetIsShowThemeSelector()
 		{
 			IsShowThemeSelector = !IsShowThemeSelector;
 			NotifyStateChanged();
 		}
+		#endregion
+
+		#region NavBar
+		public void SetNavBarSelectedMenus(string text)
+		{
+			NavBarSelectedMenus = text;
+			NotifyStateChanged();
+		}
+		#endregion
 
 		private void NotifyStateChanged() => OnChange?.Invoke();
 	}
