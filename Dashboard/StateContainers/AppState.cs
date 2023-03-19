be4d29c40ee2a1d6c35cf9c95e6c303ebc648322
atesz014 <atesz014@gmail.com>
2023-03-19 13:21:50 +0100

    add fixed navbar

--- Dashboard/StateContainers/AppState.cs
+++ Dashboard/StateContainers/AppState.cs
@@ -9,6 +9,8 @@
 		public string? SideBarColor { get; private set; }
 		public string? SideBarType { get; private set; }
 		public bool SideBarMinimize { get; private set; }
+		public string SideBarLogo { get;  set; } = "../../assets/img/logo-ct-dark.png" ;
+
 		public bool NavBarFixed { get; private set; }
 		public bool DarkMode { get; private set; }
 
@@ -30,6 +32,7 @@
 		{
 			SideBarType = type;
 			NotifyStateChanged();
+			SideBarLogo = (type == "bg-white") ? "../../assets/img/logo-ct-dark.png" : "../../assets/img/logo-ct.png";
 		}
 
 		public void SetSideBarMinimize()
