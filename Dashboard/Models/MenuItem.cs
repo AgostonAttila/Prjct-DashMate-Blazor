b789bd50d143aaf2b152575414acb22b86897f5a
atesz014 <atesz014@gmail.com>
2023-03-19 14:37:46 +0100

    add main menu active class at SideBar

--- Dashboard/Models/MenuItem.cs
+++ Dashboard/Models/MenuItem.cs
@@ -2,14 +2,20 @@
 
 namespace Dashboard.Models
 {
-    public class MenuItem
-    {
-        public string? Link { get; set; }
-        public string? NormalClass { get; set; }
-        public string? NormalTitle { get; set; }
-        public string? MiniClass { get; set; }
-        public string? MiniTitle { get; set; }
-    }
+	public class MenuItem
+	{
+		public string? Link { get; set; }
+		public string? NormalClass { get; set; }
+		public string? NormalTitle { get; set; }
+		public string? MiniClass { get; set; }
+		public string? MiniTitle { get; set; }
+	}
+
+	public class MainMenuItem
+	{
+        public string Name { get; set; }
+		public List<MenuItem> MenuItemList { get; set; } = new List<MenuItem>();
+	}
 }
 
 
