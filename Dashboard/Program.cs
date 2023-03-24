21fc1ca4add8b07b6fc98ff41c2fbcf6918c6e63
atesz014 <atesz014@gmail.com>
2023-03-24 11:54:25 +0100

    add some layout changes

--- Dashboard/Program.cs
+++ Dashboard/Program.cs
@@ -11,4 +11,7 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.
 
 builder.Services.AddSingleton<AppState>();
 
+builder.Services.AddOptions();
+builder.Services.AddAuthorizationCore();
+
 await builder.Build().RunAsync();
