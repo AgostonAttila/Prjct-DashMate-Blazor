967d13cc9514d6092d1b66ce8c5064b23edcf96e
atesz014 <atesz014@gmail.com>
2023-03-24 13:33:49 +0100

    add authorizeView

--- Dashboard/Program.cs
+++ Dashboard/Program.cs
@@ -1,5 +1,7 @@
+using Blazored.LocalStorage;
 using Dashboard;
 using Dashboard.StateContainers;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -9,9 +11,17 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+//LocalStorage
+builder.Services.AddBlazoredLocalStorage();
+
+//Services
+
+//State containers
 builder.Services.AddSingleton<AppState>();
 
+//Authorization - Authentication
 builder.Services.AddOptions();
 builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
 
 await builder.Build().RunAsync();
