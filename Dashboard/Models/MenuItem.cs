a39b623aff7dd203aa68b9fa8355508a78bbf0d5
atesz014 <atesz014@gmail.com>
2023-03-30 23:03:09 +0200

    fix sidebar logic

--- Dashboard/Models/MenuItem.cs
+++ Dashboard/Models/MenuItem.cs
@@ -5,11 +5,8 @@ namespace Dashboard.Models
 	public class MenuItem
 	{
 		public string Id { get; set; }
-		public string? Link { get; set; }
-		public string? NormalClass { get; set; }
-		public string? Title { get; set; }
-		public string? MiniClass { get; set; }
-		public string? MiniTitle { get; set; }
+		public string? Link { get; set; }	
+		public string? Title { get; set; }		
 		public List<MenuItem> MenuItemList { get; set; } = new List<MenuItem>();
 	}
 
@@ -18,8 +15,7 @@ namespace Dashboard.Models
 		public int SequenceId { get; set; }
 		public string? Id { get; set; }
 		public string? Name { get; set; }
-		public string? Icon { get; set; }
-		public string? Css { get; set; }
+		public string? Icon { get; set; }	
 		public string? Type { get; set; }
 		public List<MenuItem> MainMenuItemList { get; set; } = new List<MenuItem>();
 
@@ -27,8 +23,7 @@ namespace Dashboard.Models
 
 
 	public class MainMenuItem
-	{
-		public string Id { get; set; }
+	{	
 		public string Name { get; set; }
 		public List<MenuItem> MenuItemList { get; set; } = new List<MenuItem>();
 	}
